b8c2406ca82bed7a8dd25eda64109f26325bbe48
massaiTHEdog <massai77@hotmail.fr>
2020-10-07 19:43:18 +0200

    Removed services

--- ScreenTemperature/MainWindow.xaml.cs
+++ ScreenTemperature/MainWindow.xaml.cs
@@ -16,14 +16,14 @@ using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 using ScreenTemperature.Classes;
 using Microsoft.Win32;
-using ScreenTemperature.Services;
-using ScreenTemperature.Services.Interfaces;
 using Color = System.Windows.Media.Color;
 using Container = SimpleInjector.Container;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 using MessageBox = System.Windows.MessageBox;
 using TimeoutException = System.TimeoutException;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ScreenTemperature
 {
@@ -54,6 +54,7 @@ namespace ScreenTemperature
 		private int _imageSliderValue = 1000;
 		private Color _selectedColor = new Color() { A = 255, R = 255, G = 186, B = 127 };
 		private BitmapImage _imageGradient;
+		private string _configDirectory;
 
 		#region Commands
 
@@ -64,14 +65,6 @@ namespace ScreenTemperature
 		public ICommand MoveConfigDownCommand { get; private set; }
 
 		#endregion
-
-		#region Services
-
-		private readonly IConfigService _configService;
-		private readonly IScreenColorService _temperatureService;
-
-		#endregion
-
 
 		#endregion
 
@@ -88,7 +81,7 @@ namespace ScreenTemperature
 				_kelvinValue = value;
 				NotifyPropertyChanged(nameof(KelvinValue));
 
-				_temperatureService.ChangeScreenColorFromKelvin(value, SelectedMonitor);
+				ApplyKelvinToMonitor(value, SelectedMonitor);
 			}
 		}
 
@@ -132,7 +125,7 @@ namespace ScreenTemperature
 				if (value != null)
 				{
 					TextNameConfig = value.ConfigName;
-					_temperatureService.ChangeScreenColorFromConfig(value, Monitors);
+					ApplySelectedConfig();
 				}
 				else
 				{
@@ -272,7 +265,7 @@ namespace ScreenTemperature
 				_imageSliderValue = value;
 				NotifyPropertyChanged(nameof(ImageSliderValue));
 
-				_temperatureService.ChangeScreenColorFromImage(value, SelectedMonitor, ImageGradient);
+				ApplySelectedImageSliderValueToSelectedMonitor();
 			}
 		}
 
@@ -335,20 +328,18 @@ namespace ScreenTemperature
 		[DllImport("gdi32.dll")]
 		private static extern IntPtr CreateDC(string lpszDriver, string lpszDevice, string lpszOutput, IntPtr lpInitData);
 
+		[DllImport("GDI32.dll")]
+		private static extern unsafe bool SetDeviceGammaRamp(int hdc, void* ramp);
+
+		[DllImport("GDI32.dll")]
+		private static extern unsafe bool GetDeviceGammaRamp(int hdc, void* ramp);
+
 		#endregion
 
 		#region Constructor
 
 		public MainWindow()
 		{
-			var container = new Container();
-			container.Register<IConfigService, ConfigService>();
-			container.Register<IScreenColorService, ScreenColorService>();
-			container.Verify();
-
-			_configService = container.GetInstance<IConfigService>();
-			_temperatureService = container.GetInstance<IScreenColorService>();
-
 			var exists = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)).Length > 1;
 
 			if (exists)//If this program already has an instance
@@ -410,17 +401,17 @@ namespace ScreenTemperature
 
 		private void SystemEvents_DisplaySettingsChanged(object sender, EventArgs e)
 		{
-			_temperatureService.ChangeScreenColorFromConfig(SelectedConfig, Monitors);
+			ApplySelectedConfig();
 		}
 
 		private void SystemEvents_PaletteChanged(object sender, EventArgs e)
 		{
-			_temperatureService.ChangeScreenColorFromConfig(SelectedConfig, Monitors);
+			ApplySelectedConfig();
 		}
 
 		private void SystemEvents_UserPreferenceChanging(object sender, UserPreferenceChangingEventArgs e)
 		{
-			_temperatureService.ChangeScreenColorFromConfig(SelectedConfig, Monitors);
+			ApplySelectedConfig();
 		}
 
 		/// <summary>
@@ -432,7 +423,7 @@ namespace ScreenTemperature
 		{
 			if (sessionSwitchEventArgs.Reason == SessionSwitchReason.SessionUnlock || sessionSwitchEventArgs.Reason == SessionSwitchReason.SessionLogon)
 			{
-				_temperatureService.ChangeScreenColorFromConfig(SelectedConfig, Monitors);
+				ApplySelectedConfig();
 			}
 		}
 
@@ -518,7 +509,7 @@ namespace ScreenTemperature
 					}
 
 					SelectedConfig.KeyBinding = null;
-					_configService.SaveConfig(SelectedConfig);
+					SaveConfig(SelectedConfig);
 				}
 				else
 				{
@@ -528,7 +519,7 @@ namespace ScreenTemperature
 					}
 
 					SelectedConfig.KeyBinding = new KeyData(keyEventArgs.Key == Key.System ? keyEventArgs.SystemKey : keyEventArgs.Key, Keyboard.IsKeyDown(Key.LeftShift), Keyboard.IsKeyDown(Key.LeftAlt), Keyboard.IsKeyDown(Key.LeftCtrl));
-					_configService.SaveConfig(SelectedConfig);
+					SaveConfig(SelectedConfig);
 
 					var mask = SelectedConfig.KeyBinding.Alt ? (uint)0x0001 : 0;
 					mask = mask | (SelectedConfig.KeyBinding.Control ? (uint)0x0002 : 0);
@@ -549,6 +540,20 @@ namespace ScreenTemperature
 
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
+			_configDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\ScreenTemperature\Configs";
+
+			try
+			{
+				Directory.CreateDirectory(_configDirectory);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Can't create config directory.\r\nTry restarting application.\r\nError:\r\n{ex.Message}", "Error");
+				Environment.Exit(0);
+			}
+
+			#region Load monitors
+
 			Monitors = new List<Monitor>();
 
 			for (int i = 0; i < Screen.AllScreens.Length; i++)
@@ -563,18 +568,43 @@ namespace ScreenTemperature
 				});
 			}
 
+			SelectedMonitor = Monitors.FirstOrDefault();
+
+			#endregion
+
 			IsRadioButtonUseTannerHellandAlgorithmChecked = true;
 
 			SelectedColor = _selectedColor;
 
-			Configs = new ObservableCollection<Config>(_configService.GetConfigs().OrderBy(conf => conf.Order));
+			#region Load configs
+
+			var configs = new List<Config>();
+
+			foreach (var file in Directory.GetFiles(_configDirectory, "*.bin"))
+			{
+				try
+				{
+					IFormatter formatter = new BinaryFormatter();
+
+					using (Stream stream = new FileStream($@"{file}", FileMode.Open, FileAccess.Read, FileShare.Read))
+					{
+						configs.Add((Config)formatter.Deserialize(stream));
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"File {file} corrupt.\r\nCan't use this config.\r\nError:\r\n{ex.Message}", "Error");
+				}
+			}
+
+			Configs = new ObservableCollection<Config>(configs.OrderBy(conf => conf.Order));
 
 			if (Configs.Count > 0)
 			{
 				SelectedConfig = Configs[0];
 			}
 
-
+			#endregion
 
 			var key = Registry.CurrentUser.OpenSubKey(RunKeyPath, true);
 
@@ -649,9 +679,9 @@ namespace ScreenTemperature
 			if (SelectedConfigIndex < Configs.Count - 1)
 			{
 				SelectedConfig.Order++;
-				_configService.SaveConfig(SelectedConfig);
+				SaveConfig(SelectedConfig);
 				Configs[SelectedConfigIndex + 1].Order--;
-				_configService.SaveConfig(Configs[SelectedConfigIndex + 1]);
+				SaveConfig(Configs[SelectedConfigIndex + 1]);
 				Configs.Move(SelectedConfigIndex, SelectedConfigIndex + 1);
 			}
 		}
@@ -665,9 +695,9 @@ namespace ScreenTemperature
 			if (SelectedConfigIndex > 0)
 			{
 				SelectedConfig.Order--;
-				_configService.SaveConfig(SelectedConfig);
+				SaveConfig(SelectedConfig);
 				Configs[SelectedConfigIndex - 1].Order++;
-				_configService.SaveConfig(Configs[SelectedConfigIndex - 1]);
+				SaveConfig(Configs[SelectedConfigIndex - 1]);
 				Configs.Move(SelectedConfigIndex, SelectedConfigIndex - 1);
 			}
 		}
@@ -678,9 +708,9 @@ namespace ScreenTemperature
 		/// <param name="obj"></param>
 		private void SaveConfig(object obj)
 		{
-			var existingConfig = Configs.FirstOrDefault(x => x.ConfigName == TextNameConfig);//Check if this config alreay exist
+			var existingConfig = Configs.FirstOrDefault(x => x.ConfigName == TextNameConfig);//Check if this config already exists
 
-			var config = _temperatureService.SaveCurrentScreenColorToConfig(TextNameConfig, Monitors);
+			var config = SaveCurrentScreenColorToConfig();
 
 			if (config != null)
 			{
@@ -708,11 +738,274 @@ namespace ScreenTemperature
 				UnRegisterHotKey(_windowHandle, KeyInterop.VirtualKeyFromKey(SelectedConfig.KeyBinding.Key));
 			}
 
-			if (_configService.DeleteConfig(SelectedConfig))
+			try
 			{
-				Configs.RemoveAt(SelectedConfigIndex);
+				File.Delete(SelectedConfig.ConfigPath);
+				Configs.Remove(SelectedConfig);
 				SelectedConfigIndex = 0;
 			}
+			catch (Exception)
+			{
+				
+			}
+		}
+
+		private unsafe void ApplySelectedConfig()
+		{
+			ushort* gArray = stackalloc ushort[3 * 256];
+
+			foreach (var monitor in Monitors)
+			{
+				ApplyKelvinToMonitor(6600, monitor);
+			}
+
+			foreach (var monitor in SelectedConfig.Monitors)
+			{
+				for (var i = 0; i < 256 * 3; i++)
+				{
+					//gArray[i] = monitor.Rgb[i];
+				}
+
+				SetDeviceGammaRamp(monitor.Hdc.ToInt32(), gArray);
+			}
+		}
+
+		/// <summary>
+		/// Changes screen color from kelvin value
+		/// Thanks to Tanner Helland for his algorithm http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+		/// </summary>
+		private unsafe void ApplyKelvinToMonitor(int value, Monitor monitor)
+		{
+			float kelvin = value;
+			var temperature = kelvin / 100;
+
+			float red, green, blue;
+
+			if (temperature <= 66)
+			{
+				red = 255;
+			}
+			else
+			{
+				red = temperature - 60;
+				red = 329.698727446f * ((float)Math.Pow(red, -0.1332047592));
+				if (red < 0) red = 0;
+				if (red > 255) red = 255;
+			}
+
+			if (temperature <= 66)
+			{
+				green = temperature;
+				green = 99.4708025861f * (float)Math.Log(green) - 161.1195681661f;
+				if (green < 0)
+				{
+					green = 0;
+				}
+
+				if (green > 255)
+				{
+					green = 255;
+				}
+			}
+			else
+			{
+				green = temperature - 60;
+				green = 288.1221695283f * ((float)Math.Pow(green, -0.0755148492));
+
+				if (green < 0) green = 0;
+				if (green > 255) green = 255;
+			}
+
+
+			if (temperature >= 66)
+			{
+				blue = 255;
+			}
+			else
+			{
+				if (temperature <= 19)
+				{
+					blue = 0;
+				}
+				else
+				{
+					blue = temperature - 10;
+					blue = 138.5177312231f * (float)Math.Log(blue) - 305.0447927307f;
+					if (blue < 0) blue = 0;
+					if (blue > 255) blue = 255;
+				}
+			}
+
+			if (value == 6600)
+			{
+				red = 255;
+				green = 255;
+				blue = 255;
+			}
+
+			ushort* gArray = stackalloc ushort[3 * 256];
+
+			for (var ik = 0; ik < 256; ik++)
+			{
+				gArray[ik] = (ushort)(ik * red);
+				gArray[256 + ik] = (ushort)(ik * green);
+				gArray[512 + ik] = (ushort)(ik * blue);
+			}
+
+			SetDeviceGammaRamp(monitor.Hdc.ToInt32(), gArray);
+		}
+
+		private unsafe Config SaveCurrentScreenColorToConfig()
+		{
+			var configToModify = Configs.FirstOrDefault(x => x.ConfigName == TextNameConfig);
+
+			//If the config doesn't exist
+			if (configToModify == null)
+			{
+				configToModify = new Config()
+				{
+					ConfigName = TextNameConfig == "" ? "config" : TextNameConfig,
+					Monitors = new List<Monitor>(),
+					Order = Configs.Count
+				};
+			}
+			//If the config already exists
+			else
+			{
+				if (MessageBox.Show($"Are you sure you want to erase this config : {TextNameConfig}?", "", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+				{
+					configToModify.Monitors = new List<Monitor>();
+				}
+				else
+				{
+					return null;
+				}
+			}
+
+			foreach (var monitor in Monitors)
+			{
+				ushort* gArray = stackalloc ushort[3 * 256];
+
+				//Get screen data
+				var retVal = GetDeviceGammaRamp(monitor.Hdc.ToInt32(), gArray);
+
+				//If it' ok
+				if (retVal)
+				{
+					List<ushort> rgb = new List<ushort>();
+
+					for (var i = 0; i < 256 * 3; i++)
+					{
+						rgb.Add(gArray[i]);
+					}
+
+					configToModify.Monitors.Add(new Monitor()
+					{
+						Label = monitor.Label,
+						//Rgb = rgb.ToArray()
+					});
+				}
+				else
+				{
+					MessageBox.Show("Can't get screen data. \r\nTry again or restart application.", "Error");
+
+					return null;
+				}
+			}
+
+			return SaveConfig(configToModify);
+		}
+
+		/// <summary>
+		/// Changes screen color from an image
+		/// </summary>
+		private unsafe void ApplySelectedImageSliderValueToSelectedMonitor()
+		{
+			float red, green, blue;
+
+			using (var outStream = new MemoryStream())
+			{
+				BitmapEncoder enc = new BmpBitmapEncoder();
+				enc.Frames.Add(BitmapFrame.Create(ImageGradient));
+				enc.Save(outStream);
+
+				using (var bmp = new Bitmap(outStream))
+				{
+					if (ImageSliderValue < 0)
+						ImageSliderValue = 0;
+					if (ImageSliderValue >= bmp.Width)
+						ImageSliderValue = bmp.Width - 1;
+
+
+					var clr = bmp.GetPixel(ImageSliderValue, 0);
+
+					red = clr.R;
+					green = clr.G;
+					blue = clr.B;
+				}
+			}
+
+			ushort* gArray = stackalloc ushort[3 * 256];
+
+			for (var ik = 0; ik < 256; ik++)
+			{
+				gArray[ik] = (ushort)(ik * red);
+				gArray[256 + ik] = (ushort)(ik * green);
+				gArray[512 + ik] = (ushort)(ik * blue);
+			}
+
+			SetDeviceGammaRamp(SelectedMonitor.Hdc.ToInt32(), gArray);
+		}
+
+		/// <summary>
+		/// Saves the config
+		/// </summary>
+		private Config SaveConfig(Config config)
+		{
+			var addConfigToList = false;
+
+			if (string.IsNullOrEmpty(config.ConfigPath))//if this config doesn't exist
+			{
+				var i = 0;
+
+				while (true)
+				{
+					if (!File.Exists($@"{_configDirectory}\config{i}.bin"))
+					{
+						using (var fs = File.Create($@"{_configDirectory}\config{i}.bin"))
+						{
+							fs.Close();
+						}
+
+						config.ConfigPath = $@"{_configDirectory}\config{i}.bin";
+						addConfigToList = true;
+						break;
+					}
+
+					i++;
+				}
+
+				config.ConfigName = string.IsNullOrEmpty(config.ConfigName) ? $"config{i}" : config.ConfigName;
+			}
+
+			try
+			{
+				IFormatter formatter = new BinaryFormatter();
+
+				using (Stream stream = new FileStream(config.ConfigPath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+				{
+					formatter.Serialize(stream, config);
+				}
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show("Impossible d'enregistrer le fichier config.\r\nErreur:\r\n" + exception.Message, "Erreur");
+			}
+
+			if (addConfigToList)
+				Configs.Add(config);
+
+			return config;
 		}
 
 		#endregion
