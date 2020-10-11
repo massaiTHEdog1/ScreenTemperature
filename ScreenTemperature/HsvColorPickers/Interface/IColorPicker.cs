5f5d8cc3ab8e05ae2d25f80be15e426a7c99f784
massaiTHEdog <massai77@hotmail.fr>
2020-10-11 23:09:59 +0200

    Added HSV wheel from https://github.com/ysdy44/HsvColorPickers-Nuget-UWP

---
+++ ScreenTemperature/HsvColorPickers/Interface/IColorPicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HSVColorPickers
+{
+    /// <summary>
+    /// Represents a basic Interface of color picker.
+    /// </summary>
+    public interface IColorPicker
+    {
+        /// <summary> Gets picker's type name. </summary>
+        string Type { get; }
+
+        /// <summary> Gets picker self. </summary>
+        Control Self { get; }
+
+        /// <summary> Gets or sets picker's color. </summary>
+        Color Color { get; set; }
+
+        /// <summary> Occurs when the color value changed. </summary>
+        event ColorChangeHandler ColorChanged;
+        /// <summary> Occurs when the color change starts. </summary>
+        event ColorChangeHandler ColorChangeStarted;
+        /// <summary> Occurs when color change. </summary>
+        event ColorChangeHandler ColorChangeDelta;
+        /// <summary> Occurs when the color change is complete. </summary>
+        event ColorChangeHandler ColorChangeCompleted;
+    }
+}
---
+++ ScreenTemperature/HsvColorPickers/Interface/IHSVPicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+
+namespace HSVColorPickers
+{
+    /// <summary>
+    /// Represents a basic Interface of HSV picker.
+    /// </summary>
+    public interface IHSVPicker
+    {
+        /// <summary> Gets picker's type name. </summary>
+        string Type { get; }
+
+        /// <summary> Gets picker self. </summary>
+        Control Self { get; }
+
+        /// <summary> Gets or sets picker's hsv. </summary>
+        HSV HSV { get; set; }
+
+        /// <summary> Occurs when the hsv value changed. </summary>
+        event HSVChangeHandler HSVChanged;
+        /// <summary> Occurs when the hsv change starts. </summary>
+        event HSVChangeHandler HSVChangeStarted;
+        /// <summary> Occurs when hsv change. </summary>
+        event HSVChangeHandler HSVChangeDelta;
+        /// <summary> Occurs when the hsv change is complete. </summary>
+        event HSVChangeHandler HSVChangeCompleted;
+    }
+}
